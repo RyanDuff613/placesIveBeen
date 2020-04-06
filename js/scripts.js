9411c716fa4b42532802e2b819613184b9e84284
Ryan Duff <ryanduff613@gmail.com>
2020-04-06 16:01:11 -0700

    last cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ function Place(country,city,timeOfYear,notes,landmarks){
   this.timeOfYear = timeOfYear;
   this.notes = notes;
   this.landmarks = landmarks; 
-  this.link = link;
 }
 
-var place1 = new Place('italy', 'bergamo', 'winter', 'off the radar, low-key classic northern Italy',['citta alta','wineries','lake como', 'train to milano'].'https://www.visitbergamo.net/');
+var place1 = new Place('italy', 'bergamo', 'winter', 'off the radar, low-key classic northern Italy',['citta alta','wineries','lake como', 'train to milano']);
